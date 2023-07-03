40334d6e061edd9f67efa49f3d227ca5cf11f393
Daniel <danielytan1@gmail.com>
2023-07-04 00:27:03 +0900

    integrated console into console tab

--- src/app/console/page.tsx
+++ src/app/console/page.tsx
@@ -1,5 +1,28 @@
-import Link from 'next/link'
- 
-export default function Page() {
-  return <Link href="/">Home</Link>
-}
+"use client"
+
+import React, { useEffect, useRef } from 'react';
+
+const AdminConsoleWrapper: React.FC = () => {
+  const adminConsoleRef = useRef<HTMLDivElement>(null);
+  const isAppendedRef = useRef(false);
+
+  useEffect(() => {
+    import('@/components/admin-console').then(({ default: AdminConsole }) => {
+      const adminConsole = new AdminConsole();
+      if (adminConsoleRef.current && !isAppendedRef.current) {
+        adminConsoleRef.current.appendChild(adminConsole);
+        isAppendedRef.current = true;
+      }
+    });
+  }, []);
+
+  return <div ref={adminConsoleRef}></div>;
+};
+
+const ConsolePage: React.FC = () => (
+  <div>
+    <AdminConsoleWrapper />
+  </div>
+);
+
+export default ConsolePage;
