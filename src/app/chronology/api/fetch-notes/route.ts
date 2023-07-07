baa75e3c63ee5f2c9e9ddc47bafe368113685068
Daniel <danielytan1@gmail.com>
2023-07-07 14:57:09 +0900

    revalidating path on fetch-notes

--- src/app/chronology/api/fetch-notes/route.ts
+++ src/app/chronology/api/fetch-notes/route.ts
@@ -1,8 +1,12 @@
 import connectToDatabase from '../../lib/note-mongodb';
 import { NextRequest, NextResponse } from 'next/server';
+import { revalidatePath } from 'next/cache'
 
 export async function GET(request: NextRequest) {
   try {
+    const path = request.nextUrl.searchParams.get('path') || '/'
+    revalidatePath(path)
+
     // Connect to the MongoDB database
     const db = await connectToDatabase();
 
