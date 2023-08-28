e2d942bd49e8031f45330786492d3a73465d8ca0
Daniel <danielytan1@gmail.com>
2023-08-28 13:21:34 +0900

    added sidebar to chronology (needs editing)

--- src/app/chronology/layout.tsx
+++ src/app/chronology/layout.tsx
@@ -1,14 +1,22 @@
-export default function DashboardLayout({
-  children, // will be a page or nested layout
-}: {
+import { docsConfig } from "./config/docs"
+import { DocsSidebarNav } from "@/components/sidebar-nav"
+import { ScrollArea } from "@/components/ui/scroll-area"
+
+interface DocsLayoutProps {
   children: React.ReactNode
-}) {
+}
+
+export default function DocsLayout({ children }: DocsLayoutProps) {
   return (
-    <section>
-      {/* Include shared UI here e.g. a header or sidebar */}
-      <nav></nav>
- 
-      {children}
-    </section>
+    <div className="border-b">
+      <div className="px-4 md:px-8 lg:px-12 flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
+        <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
+          <ScrollArea className="h-full py-6 pl-0 pr-0 lg:py-8">
+            <DocsSidebarNav items={docsConfig.sidebarNav} />
+          </ScrollArea>
+        </aside>
+        {children}
+      </div>
+    </div>
   )
 }
